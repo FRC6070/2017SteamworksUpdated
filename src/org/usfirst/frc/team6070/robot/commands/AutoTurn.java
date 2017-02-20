057fd80c4e9f043e8483d603536f407e6db8f9eb
nafehshoaib <nafehshoaib14@gmail.com>
2017-02-20 11:36:34 -0500

    autoturn fix 2

--- src/org/usfirst/frc/team6070/robot/commands/AutoTurn.java
+++ src/org/usfirst/frc/team6070/robot/commands/AutoTurn.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package org.usfirst.frc.team6070.robot.commands;
 
 import org.usfirst.frc.team6070.robot.Robot;
@@ -6,62 +6,6 @@ import org.usfirst.frc.team6070.robot.Robot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 
-/**
- *
- */
-public class AutoTurn extends Command {
-
-	double angle;
-	boolean done = false;
-	Timer mytime;
-    public AutoTurn(double angle) {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    	requires(Robot.DriveBase);
-    	this.angle = angle;
-    	mytime = new Timer();
-    	mytime.reset();
-    	mytime.start();
-    }
-
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    	//Robot.DriveBase.resetGyro();
-    }
-
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    	Robot.DriveBase.setdirection();
-    	//Robot.DriveBase.resetGyro();
-    	Robot.DriveBase.turnPID(angle, 0.6);
-    	//Robot.DriveBase.resetAccel();
-    }
-
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return done || (mytime.get() > 3);
-    }
-
-    // Called once after isFinished returns true
-    protected void end() {
-    	//Robot.DriveBase.resetGyro();
-       	Robot.DriveBase.accelPID.resetPID();
-       	Robot.DriveBase.gyroPID.resetPID();
-    }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    	end();
-    }
-}
-=======
-package org.usfirst.frc.team6070.robot.commands;
-
-import org.usfirst.frc.team6070.robot.Robot;
-
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
@@ -112,4 +56,4 @@ public class AutoTurn extends Command {
     	end();
     }
 }
->>>>>>> refs/remotes/origin/master
+
